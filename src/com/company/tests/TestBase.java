deab4350140ba696e73bfcfe4fcacd4669ef7857
valerya-kozyrev <lerkucij@gmail.com>
2021-07-03 13:53:42 +0300

    2021.07.01

---
+++ src/com/company/tests/TestBase.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    public static final String PASSWORD = "trello0909";
+    public static final String LOGIN = "lerkucij@gmail.com";
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void startUp() {
+        driver = new ChromeDriver();
+        driver.get("https://trello.com");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+    public void editField(WebElement field, String value) {
+        field.click();
+        field.sendKeys(value);
+    }
+
+    public void waitUntilElementIsClickable(By locator, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.elementToBeClickable(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitUntilElementIsVisible(By locator, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.visibilityOfElementLocated(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void waitUntilAllElementsArePresent(By locator, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
